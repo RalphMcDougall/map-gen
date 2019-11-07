bee43c60169927c5f3c4dc1900f8595b968890da
Ralph McDougall <ralphmcdougall2000@gmail.com>
2019-11-07 23:36:49 +0200

    Added SFML GUI loop stuff

--- main.cpp
+++ main.cpp
@@ -1,42 +1,76 @@
 #include <iostream>
+#include <string>
 #include <SDL2/SDL.h>
 
 using namespace std;
 
 void log(const string msg);
+int getRand(int lower, int upper);
 
-const unsigned int SCREEN_WIDTH = 800;
-const unsigned int SCREEN_HEIGHT = 600;
-const string WINDOW_CAPTION = "MAP GEN";
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const char* const SCREEN_CAPTION = "MAP GEN";
 
-SDL_Window* window = NULL;
-SDL_Surface* screenSurface = NULL;
+SDL_Renderer *renderer;
+SDL_Window *window;
 
-int main()
+int main(void) 
 {
     log("Program started");
+    srand(0);
 
-    log("Creating SDL objects");
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    log("Initialising SDL objects");
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+    SDL_SetWindowTitle(window, SCREEN_CAPTION);
+
+    log("Starting GUI loop");
+    bool running = true;
+    while (running)
     {
-        // Error in initialisation
-        log("SDL could not be initialised!");
-        cout << SDL_GetError() << endl;
-        return 0;
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        for (int y = 0; y < SCREEN_HEIGHT; ++y)
+        {
+            for (int x = 0; x < SCREEN_WIDTH; ++x)
+            {
+                SDL_SetRenderDrawColor(renderer, x % 256, y % 256, (x + y) % 256, 0);
+                SDL_RenderDrawPoint(renderer, x, y);
+            }
+        }
+
+        while (SDL_PollEvent(&event) != 0)
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        } 
+
+        SDL_RenderPresent(renderer);
     }
 
-    window = SDL_CreateWindow(WINDOW_CAPTION, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL)
-    {
-        log("Couldn't create window");
-        cout << SDL_GetError() << endl;
-        return 0;
-    }
-    log("Program closed");
-    return 0;
+    // Free assets
+    log("Freeing assets");
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    log("Program ended");
+    return EXIT_SUCCESS;
 }
 
 void log(const string msg)
 {
     cout << msg << endl;
+}
+
+int getRand(int lower, int upper)
+{
+    // return a random number in the range [lower, upper)
+    int r = rand();
+    return lower + r % (upper - lower);
 }
