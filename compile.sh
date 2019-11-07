ed25326821e73c00e85f330faf67ad99d4372bb1
Ralph McDougall <ralphmcdougall2000@gmail.com>
2019-11-07 22:51:21 +0200

    Changed from SFML to SDL2

--- compile.sh
+++ compile.sh
@@ -1,1 +1,5 @@
-g++ main.cpp -std=c++11 -O2 -o map-gen
+
+echo "Creating main object"
+g++ -c main.cpp -std=c++11 -O2
+echo "Linking objects together"
+g++ main.o -o map-gen -w -lSDL2 -std=c++11 -O2
--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 using namespace std;
 
+void log(const string msg);
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+const string WINDOW_CAPTION = "MAP GEN";
+
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+
 int main()
 {
-    cout << "Working" << endl;
+    log("Program started");
+
+    log("Creating SDL objects");
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        // Error in initialisation
+        log("SDL could not be initialised!");
+        cout << SDL_GetError() << endl;
+        return 0;
+    }
+
+    window = SDL_CreateWindow(WINDOW_CAPTION, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        log("Couldn't create window");
+        cout << SDL_GetError() << endl;
+        return 0;
+    }
+    log("Program closed");
     return 0;
+}
+
+void log(const string msg)
+{
+    cout << msg << endl;
 }
