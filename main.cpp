81ed49cdf64577ad2785602f0b8fc055bd93bc16
Ralph McDougall <ralphmcdougall2000@gmail.com>
2019-11-08 00:06:22 +0200

    Added function to write pixel data to image

--- main.cpp
+++ main.cpp
@@ -6,13 +6,16 @@ using namespace std;
 
 void log(const string msg);
 int getRand(int lower, int upper);
+void writePixelData(SDL_Surface *image);
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const char* const SCREEN_CAPTION = "MAP GEN";
 
-SDL_Renderer *renderer;
-SDL_Window *window;
+SDL_Renderer *renderer = NULL;
+SDL_Window *window = NULL;
+SDL_Surface *image = NULL;
+SDL_Texture *texture = NULL;
 
 int main(void) 
 {
@@ -26,21 +29,18 @@ int main(void)
     SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
     SDL_SetWindowTitle(window, SCREEN_CAPTION);
 
+    image = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0);
+    writePixelData(image);
+    
+
+    texture = SDL_CreateTextureFromSurface(renderer, image);
+
     log("Starting GUI loop");
     bool running = true;
     while (running)
     {
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer);
-
-        for (int y = 0; y < SCREEN_HEIGHT; ++y)
-        {
-            for (int x = 0; x < SCREEN_WIDTH; ++x)
-            {
-                SDL_SetRenderDrawColor(renderer, x % 256, y % 256, (x + y) % 256, 0);
-                SDL_RenderDrawPoint(renderer, x, y);
-            }
-        }
 
         while (SDL_PollEvent(&event) != 0)
         {
@@ -50,11 +50,14 @@ int main(void)
             }
         } 
 
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
     }
 
     // Free assets
     log("Freeing assets");
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(image);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
@@ -73,4 +76,20 @@ int getRand(int lower, int upper)
     // return a random number in the range [lower, upper)
     int r = rand();
     return lower + r % (upper - lower);
+}
+
+void writePixelData(SDL_Surface *image)
+{
+    for (int y = 0; y < SCREEN_HEIGHT; ++y)
+    {
+        for (int x = 0; x < SCREEN_WIDTH; ++x)
+        {
+            unsigned char* pixels = (unsigned char*) image->pixels;
+            int c = getRand(0, 256);
+            pixels[ 4 * (y * image->w + x) + 0 ] = c;
+            pixels[ 4 * (y * image->w + x) + 1 ] = c;
+            pixels[ 4 * (y * image->w + x) + 2 ] = c;
+            pixels[ 4 * (y * image->w + x) + 3 ] = 255;
+        }
+    }
 }
